a3c70db5ce4c3a6501487f0255cb8822a520e7fa
KikeValeroSoriano <kikevalerosoriano@gmail.com>
2024-12-23 20:39:40 +0100

    User -> Kike Valero - Provant

--- jenkinsScripts/indexUpdateReadme.js
+++ jenkinsScripts/indexUpdateReadme.js
@@ -6,13 +6,10 @@ async function main() {
         // console.log('Mostrem el directori actual de treball:', process.cwd());
 
         const resultat_test = process.argv[2];
-        let counter = 1;
         const img_error = 'https://img.shields.io/badge/test-failure-red';
         const img_exit = 'https://img.shields.io/badge/tested%20with-Cypress-04C38E.svg';
         const badge = resultat_test === '0' ? img_exit : img_error;
-        const missatge_badge = `RESULTAT DELS ÚLTIMS TESTS ${counter}\n ![Test result badge](${badge})`;
-
-        counter++;
+        const missatge_badge = `RESULTAT DELS ÚLTIMS TESTS \n ![Test result badge](${badge})`;
 
         const oldReadmePath = './OldREADME.md';
         let oldReadmeContent = await fs.readFile(oldReadmePath, 'utf-8');
