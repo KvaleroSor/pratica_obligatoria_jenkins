b9eb3d2dcce806415f852abec1d3de954cded8ed
KikeValeroSoriano <kikevalerosoriano@gmail.com>
2024-12-23 08:22:31 +0100

    User -> Kike Valero - Corregint errors condicional

--- jenkinsScripts/indexLinter.js
+++ jenkinsScripts/indexLinter.js
@@ -3,7 +3,7 @@ const result = process.argv[2];
 console.log(result);
 
 if(result !== 0){
-    console.log('Linter ha encontrado errores ...❌');
-}else{
-    console.log('Linter no ha enctronado ningún error ...✅');
+    console.log('Linter ha encontrado errores... ❌');
+}else if(result === 0){
+    console.log('Linter no ha enctronado ningún error... ✅');
 }
