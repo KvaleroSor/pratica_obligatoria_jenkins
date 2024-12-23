0d7d9758df8e78bbb8058fec498f55d501217cc2
KikeValeroSoriano <kikevalerosoriano@gmail.com>
2024-12-23 08:19:11 +0100

    User -> Kike Valero - Provant

--- jenkinsScripts/indexLinter.js
+++ jenkinsScripts/indexLinter.js
@@ -1,5 +1,7 @@
 const result = process.argv[2];
 
+console.log(result);
+
 result !== 0
     ? console.log("Linter ha encontrado errors...")
     : console.log("Linter ha finalizado sin errores...");
