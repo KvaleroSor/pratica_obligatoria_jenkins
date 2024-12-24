312a60819cbc8b99dff2ee5a08e8fafc418c7ac1
KikeValeroSoriano <kikevalerosoriano@gmail.com>
2024-12-24 19:29:48 +0100

    Proves

--- jenkinsScripts/indexLinter.js
+++ jenkinsScripts/indexLinter.js
@@ -3,7 +3,7 @@ const result = process.argv[2];
 console.log('Result en l´arxiu indexLinter -> ' + result);
 
 console.log('Resultat -> ' + result);
-if (result.includes('No ESLint warnings or errors')) {
+if (result.toString() == '0') {
     console.log('Linter no ha encontrado ningún error... ✅');
 } else {
     console.log('Linter ha encontrado errores... ❌');
